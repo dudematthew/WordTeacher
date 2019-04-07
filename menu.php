301b2b957fce939e1cd89268262f05796f0e2be3
Mateusz <mmoczydlowski4@gmail.com>
2019-04-07 10:58:43 +0200

    Removed images from menu

--- menu.php
+++ menu.php
@@ -8,19 +8,16 @@
 
     <div class="steps">
         <div class="steps__step">
-                <p>1. Przepisz słówka do pliku .txt</p>
+                <h2>1. Przepisz słówka do pliku .txt</h2>
                 <p class="--info_text">Jak to poprawnie zrobić dowiesz się <a href="./textSyntax.php">tutaj</a>.</p>
-                <img src="./img/sample_text.png" alt="Przykładowe słówka" class="steps__step__img">
         </div>
         <div class="steps__step">
-                <p>2. Załaduj plik</p>
+                <h2>2. Załaduj plik</h2>
                 <p class="--info_text">Przejdź do zakładki <a href="./addFileToLoad.php">Załaduj nowy plik</a>.</p>
-                <img src="./img/load_file.png" alt="Załadowywanie pliku" class="steps__step__img">
         </div>
         <div class="steps__step">
-                <p>1. Pozwól się przepytać</p>
+                <h2>1. Pozwól się przepytać</h2>
                 <p class="--info_text">Przejdź do zakładki <a href="./teachMe.php">Ucz mnie</a>, lub <a href="./pollMe.php">Przepytaj mnie</a>.</p>
-                <img src="./img/sample_question.png" alt="Przykładowe pytanie" class="steps__step__img">
         </div>
     </div>
     <a href="./addFileToLoad.php">
