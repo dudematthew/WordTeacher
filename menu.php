1f6d79614b3a13523f090e770f2628c2e563b8bf
dudematthew <mmoczydlowski4@gmail.com>
2024-06-20 22:45:53 +0200

    Fix another textSyntaxTutorial link

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <?php
-    $title = "WordTeacher v1.4a";
-    include_once("./includes/header.php");
+$title = "WordTeacher v1.4a";
+include_once("./includes/header.php");
 ?>
 
 <div class="--vertical_center">
@@ -10,16 +10,16 @@
 
     <div class="steps">
         <div class="steps__step">
-                <h2>1. Przepisz słówka do pliku .txt</h2>
-                <p class="--info_text">Jak to poprawnie zrobić dowiesz się <a href="./textSyntax.php">tutaj</a>.</p>
+            <h2>1. Przepisz słówka do pliku .txt</h2>
+            <p class="--info_text">Jak to poprawnie zrobić dowiesz się <a href="./textSyntaxTutorial.php">tutaj</a>.</p>
         </div>
         <div class="steps__step">
-                <h2>2. Załaduj plik</h2>
-                <p class="--info_text">Przejdź do zakładki <a href="./addFileToLoad.php">Załaduj nowy plik</a>.</p>
+            <h2>2. Załaduj plik</h2>
+            <p class="--info_text">Przejdź do zakładki <a href="./addFileToLoad.php">Załaduj nowy plik</a>.</p>
         </div>
         <div class="steps__step">
-                <h2>3. Pozwól się przepytać</h2>
-                <p class="--info_text">Przejdź do zakładki <a href="./teachMe.php">Ucz mnie</a>, lub <a href="./pollMe.php">Przepytaj mnie</a>.</p>
+            <h2>3. Pozwól się przepytać</h2>
+            <p class="--info_text">Przejdź do zakładki <a href="./teachMe.php">Ucz mnie</a>, lub <a href="./pollMe.php">Przepytaj mnie</a>.</p>
         </div>
     </div>
     <a href="./addFileToLoad.php">
